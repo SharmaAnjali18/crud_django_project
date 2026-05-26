c75098b070442c68dc4d786d2b9b0131100dddb6
anjali <anjali002alobha@gmail.com>
2026-05-26 18:02:51 +0530

    Fix template folder case sensitivity

--- mvc_crud_project/mvc_crud_project/settings.py
+++ mvc_crud_project/mvc_crud_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mvc_crud_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'Templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
