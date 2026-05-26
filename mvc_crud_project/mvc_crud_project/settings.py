61e0769986dbac098993e80d2cb3cc12bd3b21bb
anjali <anjali002alobha@gmail.com>
2026-05-26 17:11:28 +0530

    Added build file

--- mvc_crud_project/mvc_crud_project/settings.py
+++ mvc_crud_project/mvc_crud_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i-*5crk@#hgiascd6x(54qz*uh9d)2w89z5o@5&w5#r96k&1&!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
