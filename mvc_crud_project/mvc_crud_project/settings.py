b0a11312592f2fd4cb8314fa7bd2fd0818b8aa04
anjali <anjali002alobha@gmail.com>
2026-05-26 18:46:53 +0530

    Fix vercel crash

--- mvc_crud_project/mvc_crud_project/settings.py
+++ mvc_crud_project/mvc_crud_project/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -50,8 +49,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'mvc_crud_project.urls'
 
